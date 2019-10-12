7cf13e7d9eb81998aa7f9889770840a633b914a4
Hans-Kristian Koren <hanse@koren.im>
2019-10-12 18:37:59 +0200

    Make it work better as PWA

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import shortid from 'shortid';
 import { createBrowserHistory, Location } from 'history';
-
 import List from './List';
 import './index.css';
 
 const history = createBrowserHistory();
 
-const getListId = (location: Location) => location.pathname.replace(/\//g, '');
+const getListId = (location: Location) =>
+  location.pathname.replace(/\//g, '').trim();
 
 history.listen(location => {
   renderApp(location);
@@ -17,8 +18,17 @@ renderApp(history.location);
 
 function renderApp(location: Location, el = document.getElementById('root')) {
   const listId = getListId(location);
+
   if (listId.trim() === '') {
-    return render(<div>Nothing here</div>, el);
+    const storedId = window.localStorage.getItem('listId');
+    if (storedId) {
+      return history.push(storedId);
+    }
+
+    const generatedId = shortid.generate();
+    window.localStorage.setItem('listId', generatedId);
+    return history.push(generatedId);
   }
+
   render(<List listId={listId} />, el);
 }
